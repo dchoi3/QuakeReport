456d4c0917903876872112dfe30f51078d050ac2
Daniel <dchoi3@my.apsu.edu>
2017-06-12 01:42:51 -0500

    update

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.quakereport;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -32,8 +33,7 @@ import java.util.List;
 
  public class EarthquakeActivity extends AppCompatActivity {
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-    public EarthquakeJsonParser jsonParser;
-    public BackgroundDownloader downloader;
+    EarthquakeArrayAdapter earthquakeArrayAdapter;
 
     private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=4&limit=100";
 
@@ -42,99 +42,46 @@ import java.util.List;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-    }
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        earthquakeArrayAdapter = new EarthquakeArrayAdapter(this, R.layout.earthquake_customlistview, new ArrayList<Earthquake>());
+        earthquakeListView.setAdapter(earthquakeArrayAdapter);
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        downloader = new BackgroundDownloader(this);
-        downloader.execute(USGS_REQUEST_URL);
-        checkThreadStatus();
-    }
-
-    private void checkThreadStatus(){
-        boolean complete = false;
-        String jsonString;
-        ArrayList<Earthquake> earthquakes;
-
-        while(!complete){
-            if(downloader.getDownloadStatus()){
-                complete = true;
-                jsonString = downloader.getJsonResults();
-                closeDownloader();
-                if(jsonString != null){
-                    jsonParser = new EarthquakeJsonParser();
-                    jsonParser.execute(jsonString);
-                }
-            }
-
-        }
-        complete = false;
-
-        while(!complete && jsonParser!= null) {
-            if (jsonParser.checkStatus()) {
-                complete = true;
-                earthquakes = jsonParser.getEarthquakeArray();
-                closeParser();
-
-                final EarthquakeArrayAdapter earthquakeArrayAdapter = new EarthquakeArrayAdapter(this, R.layout.earthquake_customlistview, earthquakes);
-                // Set the adapter on the {@link ListView}
-                // so the list can be populated in the user interface
-                ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
-                earthquakeListView.setAdapter(earthquakeArrayAdapter);
-                earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                        Earthquake currentEarthquake = earthquakeArrayAdapter.getItem(position);
-                        Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
-                        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
-                        startActivity(websiteIntent);
-
-
-                    }
-                });
+                Earthquake currentEarthquake = earthquakeArrayAdapter.getItem(position);
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                startActivity(websiteIntent);
 
 
             }
-        }
+        });
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
 
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        closeDownloader();
-        closeParser();
-    }
 
-    private void closeDownloader(){
-        if(downloader != null){
-            downloader.cancel(true);
-            downloader = null;
-        }
-    }
-
-    private void closeParser(){
-        if(jsonParser != null){
-            jsonParser .cancel(true);
-            jsonParser  = null;
-        }
-    }
 
     private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>>{
 
 
         @Override
         protected List<Earthquake> doInBackground(String... params) {
-            return null;
+            if(params.length < 1 || params[0] == null){
+                return null;
+            }
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(params[0]);
+            return result;
         }
 
         @Override
         protected void onPostExecute(List<Earthquake> earthquakes) {
-            super.onPostExecute(earthquakes);
+            earthquakeArrayAdapter.clear();
+            if (earthquakes != null && !earthquakes.isEmpty()) {
+                earthquakeArrayAdapter.addAll(earthquakes);
+            }
         }
     }
 
